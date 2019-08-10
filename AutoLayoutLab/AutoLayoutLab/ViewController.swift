8c9469c0cb1985c1a3de2ebdc793f6118fbcd0ab
AGonzalez5195 <52180531+AGonzalez5195@users.noreply.github.com>
2019-08-09 23:56:33 -0400

    Add files via upload

---
+++ AutoLayoutLab/AutoLayoutLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutLab
+//
+//  Created by Anthony Gonzalez on 8/7/19.
+//  Copyright © 2019 Anthony Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
